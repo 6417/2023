7fcb4b684c411d5886d3578e762d1856f9b03f59
lars <hoeslilars@gmail.com>
2023-03-13 16:40:17 +0100

    invert driving direction

--- src/main/java/frc/robot/commands/driveCommands/DriveCommand.java
+++ src/main/java/frc/robot/commands/driveCommands/DriveCommand.java
@@ -40,7 +40,7 @@ public class DriveCommand extends CommandBase {
         
         joystickInputY = MathUtil.applyDeadband(joystickInputY, 0.05);
         // Call Drive::drive with the input
-        m_subsystem.driveJoystick(joystickInputY, joystickTurnValue, steerWheelInput);
+        m_subsystem.driveJoystick(joystickInputY, -joystickTurnValue, steerWheelInput);
     }
 
     // Called once the command ends or is interrupted.
